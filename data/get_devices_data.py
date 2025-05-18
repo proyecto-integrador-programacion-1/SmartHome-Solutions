fde016400e0bbca28f5eb8c959181a4f8c3fc612
Agustin Difilippo <difilippoagustin@gmail.com>
2025-05-18 19:19:21 -0300

    add 'delete' feature

--- data/get_devices_data.py
+++ data/get_devices_data.py
@@ -1,7 +1,7 @@
 from interfaces import devices_info
 
 device1 = devices_info.DeviceData("Luces cochera", False)
-device2 = devices_info.DeviceData("Luces entrada", True)
+device2 = devices_info.DeviceData("Luces entrada", True, True)
 device3 = devices_info.RecorderDeviceData("Camaras dormitorio", True, True)
 device4 = devices_info.TempratureDeviceData("Heladera quincho", True, 6)
 	
@@ -13,3 +13,11 @@ def get_devices_from_local():
 
 def set_new_device(device):
 	devices.append(device)
+
+
+def delete_device(device_name):
+	for device in devices:
+		if device.name.lower() == device_name:
+			devices.remove(device)
+			return True
+	return False
--- menu_methods/delete_device.py
+++ menu_methods/delete_device.py
@@ -1,2 +1,19 @@
+from data import get_devices_data
+
 def delete():
-	pass
+	while True:
+		print("1. Eliminar dispositivo")
+		print("2. Volver al menú principal")
+		selected_option = input("Seleccione una opción: ")
+		if selected_option == "2":
+			break
+		elif selected_option == "1":
+			device_name = input("\nEscriba el nombre del dispositivo a eliminar: \n")
+
+			device_deleted = get_devices_data.delete_device(device_name.lower())
+			if device_deleted:
+				print("Dispositivo eliminado con éxito.\n")
+			else:
+				print(f"No existe un dispositivo en el sistema con el nombre ingresado: {device_name}")
+
+		print("\n")
