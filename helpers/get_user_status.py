4abffb84cb0c2f920a5f4f17b6f1fd68a76cb614
BrunoRazzotti <brunorazzotti99@gmail.com>
2025-06-09 01:18:26 -0300

    Se agrega user helper para el status

---
+++ helpers/get_user_status.py
@@ -0,0 +1,6 @@
+def get_user_status (user):
+	print("-----------------")
+	print(f"Nombre: {user.name}")
+	print(f"Apellido: {user.subname}")
+	print(f"mail: {user.mail}")
+	print(f"Es admin o usuario: {user.is_admin}")    
